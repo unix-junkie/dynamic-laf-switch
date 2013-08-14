a9d6ea0bd9425d8cf5f626454d533032d063cd54
Andrew ``Bass'' Shcheglov <andrewbass@gmail.com>
2013-08-14 13:22:00 +0400

    ADD: initial ci

---
+++ src/main/java/com/example/package-info.java
@@ -0,0 +1,11 @@
+/*-
+ * $Id$
+ */
+
+/**
+ * @author Andrew ``Bass'' Shcheglov (mailto:andrewbass@gmail.com)
+ */
+@NonNullByDefault(true)
+package com.example;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
