62920b43c6456fd2904017e2a75f3ad8a9e67fd4
Andrew ``Bass'' Shcheglov <andrewbass@gmail.com>
2013-08-15 18:29:43 +0400

    MAJOR: backported to 1.2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,6 +8,8 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
@@ -342,7 +344,14 @@ abstract class Main {
 		menuBar.add(themeMenu);
 
 		frame.setJMenuBar(menuBar);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			/**
+			 * @see WindowAdapter#windowClosing(WindowEvent)
+			 */
+			public void windowClosing(final WindowEvent e) {
+				System.exit(0);
+			}
+		});
 
 		final JPanel contentPane = (JPanel) frame.getContentPane();
 		contentPane.setPreferredSize(new Dimension(320, 240));
